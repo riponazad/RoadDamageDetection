b0df6612ea077b8865c94763d79f7efaf8c1cca9
Md. Abulkalam Azad <39602054+riponazad@users.noreply.github.com>
2022-11-18 00:51:17 +0100

    bash updated

---
+++ scripts/cluster_temp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00
+#SBATCH --job-name=Road_Damage_Detection
+#SBATCH --output=Road_Damage_Detection.out
+#SBATCH --error=Road_Damage_Detection.err
+#SBATCH --partition=CPUQ
+#SBATCH --gres=gpu:1
+#SBATCH --account=iv-imt
+#SBATCH --nodes=1
+#SBATCH --mail-user=mdaaz@stud.ntnu.no
+
+
+python scripts/train.py --root_dir /cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway --model_name fasterrcnn_mobilenetv3_low --num_epochs 200
+python scripts/train.py --root_dir /cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway --model_name fasterrcnn_resnet50 --num_epochs 200
+python scripts/train.py --root_dir /cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway --model_name fasterrcnn_resnet50v2 --num_epochs 200
+python scripts/train.py --root_dir /cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway --model_name fasterrcnn_mobilenetv3 --num_epochs 200
